6839d0b708a8f04049e415b9bd361d7a11822baf
agrover86 <amitgrover.asg@gmail.com>
2018-01-22 08:22:19 -0500

    checkpoint 70 create rooms: completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,11 +5,11 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      rooms: []
-    };
+    this.state = {rooms: [],newRoomName: ''};
     // create a firebase reference for rooms
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
   }
 
   componentDidMount() {
@@ -20,14 +20,31 @@ class RoomList extends Component {
      });
    }
 
+   handleChange(e){
+      e.preventDefault();
+      this.setState({ newRoomName: e.target.value });
+   }
+
+   createRoom(e){
+   e.preventDefault();
+   this.roomsRef.push({ name: this.state.newRoomName});
+   this.setState({ newRoomName:''});
+ }
+
+
   render() {
     return (
-      <div>
-      { this.state.rooms.map( (room) =>
-
-            <li key={room.key}>{room.name}</li>
-      )}
-      </div>
+      <section className="chatRooms">
+        <form className="addChatRoom">
+          <input type="text" value={this.state.newRoomName} placeholder="New Room" onChange={this.handleChange}/>
+          <input type="submit" onClick={this.createRoom}/>
+        </form>
+        <div className="chatRoomsList">
+           { this.state.rooms.map( (room) =>
+              <li key={room.key}>{room.name}</li>
+           )}
+        </div>
+      </section>
    );
  }
 }
